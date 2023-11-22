0076bb59fdcc574f7119ecc8cf2eb2897015bb7f
tinobest <sayayinlenin@hotmail.com>
2023-11-22 15:50:23 -0500

    -acontadorEliminadoInfinito

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo03.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo03.java
@@ -31,8 +31,9 @@ public class Ejemplo03 {
             valor_ingresado = entrada.nextInt();
             suma = suma + valor_ingresado;
             System.out.printf("Valor ingresado %d\n", valor_ingresado);
-            contador = contador + 1;
-        }
+            //contador = contador + 1;
+        } //cliclo repetitivo, pedimos un valor ingresado para sumarlo e irlos 
+          //e ir los acomulunadoi 
 
         System.out.printf("La suma final es %d\n", suma);
 
